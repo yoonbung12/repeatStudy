672c246f7494f56f2999ae4cfee191a1263efed4
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-12-20 17:48:10 +0900

    socket: 이해가 필요

---
+++ socket-repeat/socket-re/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Chatting from './components/Chatting';
+
+function App() {
+  return (
+    <div className="App">
+      <Chatting />
+    </div>
+  );
+}
+
+export default App;
---
+++ socket-repeat/socket-re/src/components/Notice.js
@@ -0,0 +1,11 @@
+
+
+export default function Notice({chat}) {
+    return (
+        <>
+            <div className="list notice">{chat.notice}</div>
+        </>
+        
+        
+    )
+}
